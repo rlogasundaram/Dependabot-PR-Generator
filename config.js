7788bf8ba67382cdf488e244035da3c5a4e203ea
Rajsundar <c_rlogasundaram@cloudbees.com>
2023-11-28 17:08:24 +0530

    Token removed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 gitOptions = {
-    TOKEN: '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'
+    TOKEN: ''
 },
 gitAPI = {
     url: 'https://api.github.com/repos'
